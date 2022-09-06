88d4e2bc925162d8a426d43ed342f3d1a6d7acc0
Luis Rosales <luchorosalesc6@gmail.com>
2022-09-06 11:52:11 -0500

    Se terminó con el local Storage creando los dos useEffect en el App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,43 @@ import { Form } from "./Components/Form";
 import { Header } from "./Components/Header";
 import { ListaTareas } from "./Components/ListaTareas";
 
-
 function App() {
-  const [tareas, setTareas] = useState([])
-  const [tarea, setTarea] = useState({})
+  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState({});
 
-  useEffect(()=>{
-    const obtenerTareasLocalStorage=()=>{
-      const tareasLocalStorage=localStorage.getItem('tareas')
-      console.log(tareasLocalStorage)
+  useEffect(() => {
+    const obtenerTareasLocalStorage = () => {
+      const tareasLocalStorage =
+        JSON.parse(localStorage.getItem("tareas")) ?? [];
+        setTareas(tareasLocalStorage);
     };
     obtenerTareasLocalStorage();
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    localStorage.setItem('tareas',JSON.stringify( tareas ))
-  },[tareas])
+  useEffect(() => {
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+  }, [tareas]);
 
-
-  const eliminarTarea=(id)=>{
-    const actualizarTarea=tareas.filter( tarea=>tarea.id !== id );
-    setTareas(actualizarTarea)
-  }
+  const eliminarTarea = (id) => {
+    const actualizarTarea = tareas.filter((tarea) => tarea.id !== id);
+    setTareas(actualizarTarea);
+  };
 
   return (
     <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 md:flex">
-        <Form 
-        tarea={tarea}
-        tareas={tareas}//bien
-        setTareas={setTareas}//bien
-        setTarea={setTarea}
-
+        <Form
+          tarea={tarea}
+          tareas={tareas} //bien
+          setTareas={setTareas} //bien
+          setTarea={setTarea}
         />
-        <ListaTareas 
-        tareas={tareas}
-        setTarea={setTarea}
-        eliminarTarea={eliminarTarea}
+        <ListaTareas
+          tareas={tareas}
+          setTarea={setTarea}
+          eliminarTarea={eliminarTarea}
         />
-
-        
       </div>
     </div>
   );
